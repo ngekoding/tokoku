51c8c70031b5e25938d798dfdda56cfd4e1a9949
Nur Muhammad <blog.nurmuhammad@gmail.com>
2023-04-02 22:18:15 +0800

    feat: add textarea type to text-input component

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -6,6 +6,10 @@ defineProps({
         type: String,
         required: true,
     },
+    type: {
+        type: String,
+        default: 'text'
+    }
 });
 
 defineEmits(['update:modelValue']);
@@ -22,7 +26,14 @@ defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
+    <textarea
+        v-if="type === 'textarea'"
+        class="border-gray-300 focus:border-blue-500 focus:ring-blue-500 rounded-md shadow-sm"
+        @input="$emit('update:modelValue', $event.target.value)"
+        ref="input">{{ modelValue }}</textarea>
     <input
+        v-else
+        :type="type"
         class="border-gray-300 focus:border-blue-500 focus:ring-blue-500 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
